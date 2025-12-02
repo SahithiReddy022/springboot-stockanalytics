694f16a701545834362555ec8073b33b1e253859
vivek <vivek@eureka.com>
2025-12-02 18:51:50 -0500

    Class 1 * Discussed web evolution to understand why we are builing Microservices * Explained about maven project, lifecycle * Created first rest service using HelloWorldController

---
+++ src/main/java/io/endeavour/stocks/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package io.endeavour.stocks.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(value = "/hello")
+    public String sayHello(){
+        return "Hello Java Class Oct 2025";
+    }
+
+    @GetMapping(value = "/hello-name/{personName}")
+    public String sayHelloWithName(@PathVariable String personName){
+        return "Hello " + personName + " Today is " + LocalDate.now();
+    }
+}
